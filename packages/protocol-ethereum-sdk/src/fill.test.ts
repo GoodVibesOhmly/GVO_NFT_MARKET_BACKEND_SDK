a7487bed60b2a4d7dd494fdad1c2f40300746120
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-11-25 14:41:44 +0300

    trace

--- packages/protocol-ethereum-sdk/src/fill.test.ts
+++ packages/protocol-ethereum-sdk/src/fill.test.ts
@@ -6,9 +6,9 @@ import { createRaribleSdk } from "./index"
 test("fill", async () => {
 	const provider = new Web3.providers.HttpProvider("https://node-mainnet.rarible.com")
 	const web3 = new Web3(provider)
-	const from = toAddress("0x58432314d6b937c2d19f11fdfef27f4bafb6910b")
+	const from = toAddress("0x207f86e2795666365747aaf1e86a4114bebd6fab")
 	const sdk = createRaribleSdk(new Web3Ethereum({ web3, from }), "mainnet")
-	const order = await sdk.apis.order.getOrderByHash({ hash: "0xc8f18cc7ae293d3c98b9adbd16f7e6f4fd0fcbe38b345de2ccd90a4a334c8bb5" })
+	const order = await sdk.apis.order.getOrderByHash({ hash: "0xf1a7384b0eeddc41dd503ccbfaa91d2e1fc2cb35b6abd048f50452e90d2da82e" })
 	if (order.type === "RARIBLE_V2") {
 		sdk.order.fill({
 			order,
